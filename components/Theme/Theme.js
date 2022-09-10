9dbfd4640083b58266e0723b753ddb9670ffea82
Kevin Blair <kblair40@gmail.com>
2022-09-10 07:47:44 -0500

    Add theme file

---
+++ components/Theme/Theme.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import styles from "./Theme.module.scss";
+
+const Theme = () => {
+  return (
+    <div className={styles.container}>
+      <section>
+        <h3 classname={styles.container__heading}>Fonts</h3>
+      </section>
+    </div>
+  );
+};
+
+export default Theme;
