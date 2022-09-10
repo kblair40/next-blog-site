c597e637e82b64efbcc95a98fae201c36949e018
Kevin Blair <kblair40@gmail.com>
2022-09-10 15:59:03 -0500

    Finish animation for 'see more' button

--- components/Posts/Post/Post.js
+++ components/Posts/Post/Post.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Link from "next/link";
 
-import styles from "./Card.module.scss";
+import styles from "./Post.module.scss";
 
 const Post = ({ title, children }) => {
   return (
-    <div className={styles.card}>
-      <h3 className={styles.card__title}>{title}</h3>
+    <div className={styles.post}>
+      <h3 className={styles.post__title}>{title}</h3>
 
-      <div className={styles.card__body}>
-        <div className={styles.card__body__text}>{children}</div>
-        <div className={styles.card__body__link}>
+      <div className={styles.post__body}>
+        <div className={styles.post__body__text}>{children}</div>
+        <div className={styles.post__body__link}>
           <Link href="#!">See More</Link>
         </div>
       </div>
