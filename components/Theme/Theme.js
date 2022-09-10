f0430e76d4e36ab8c7e3af84ea5806dd0d4ddf67
Kevin Blair <kblair40@gmail.com>
2022-09-10 08:18:00 -0500

    Add font styles to theme page

--- components/Theme/Theme.js
+++ components/Theme/Theme.js
@@ -5,8 +5,11 @@ import styles from "./Theme.module.scss";
 const Theme = () => {
   return (
     <div className={styles.container}>
-      <section>
-        <h3 classname={styles.container__heading}>Fonts</h3>
+      <section className={styles.container__section}>
+        <h3 classname={styles.conatiner__section__heading}>Fonts</h3>
+
+        <p className={styles.text_primary}>Primary text</p>
+        <p className={styles.text_secondary}>Secondary text</p>
       </section>
     </div>
   );
