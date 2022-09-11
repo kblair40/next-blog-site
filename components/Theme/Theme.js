d21d91030b4edd39e4da089d4031a05c98f3b643
Kevin Blair <kblair40@gmail.com>
2022-09-11 12:20:06 -0500

    Fix invalid css prop

--- components/Theme/Theme.js
+++ components/Theme/Theme.js
@@ -7,7 +7,7 @@ const Theme = () => {
   return (
     <div className={styles.container}>
       <section className={styles.container__section}>
-        <h3 classname={styles.conatiner__section__heading}>Fonts</h3>
+        <h3 className={styles.conatiner__section__heading}>Fonts</h3>
 
         <p className={styles.text_primary}>Primary text</p>
         <p className={styles.text_secondary}>Secondary text</p>
