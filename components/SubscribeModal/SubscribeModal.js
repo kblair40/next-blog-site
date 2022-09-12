1441889cef616d32d89a24505d5dc79ffaf99d52
Kevin Blair <kblair40@gmail.com>
2022-09-12 17:42:20 -0500

    Finish base styles for modal

--- components/SubscribeModal/SubscribeModal.js
+++ components/SubscribeModal/SubscribeModal.js
@@ -1,29 +1,23 @@
 import React from "react";
 import Modal from "react-modal";
 
+import Input from "components/UI/Input";
 import styles from "./SubscribeModal.module.scss";
-
-const customStyles = {
-  content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-    minHeight: "200px",
-  },
-};
 
 Modal.setAppElement("#layout");
 
 const SubscribeModal = ({ isOpen, onClose }) => {
   return (
-    <Modal
-      style={customStyles}
-      isOpen={isOpen}
-      onRequestClose={onClose}
-    ></Modal>
+    <Modal className={styles.modal} isOpen={isOpen} onRequestClose={onClose}>
+      <div className={styles.modal__header}>
+        <p className={styles.modal__title}>Subscribe</p>
+      </div>
+
+      <div className={styles.modal__body}>
+        <Input />
+        <Input />
+      </div>
+    </Modal>
   );
 };
 
