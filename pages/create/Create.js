8ebd10e63338a567c5d37cc19eee53f6298fd4cf
Kevin Blair <kblair40@gmail.com>
2022-09-12 06:35:25 -0500

    Add skeleton for Input component

--- pages/create/Create.js
+++ pages/create/Create.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Editor from "./Editor";
+import Input from "components/UI/Input";
 import styles from "./Create.module.scss";
 
 const Create = () => {
@@ -8,6 +9,10 @@ const Create = () => {
     <div className={styles.container}>
       <div className={styles.container__content}>
         <Editor />
+      </div>
+
+      <div className={styles.container__input}>
+        <Input />
       </div>
     </div>
   );
