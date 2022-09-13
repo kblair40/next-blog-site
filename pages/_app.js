c23631effd110e8aeb1e348889ed9b003085eba3
Kevin Blair <kblair40@gmail.com>
2022-09-13 06:48:21 -0500

    replace css import with scss import

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 // ideally the below import is moved to _globals.scss
 import "react-textarea-markdown-editor/build/TextareaMarkdownEditor.scss";
-import "react-toastify/dist/ReactToastify.css";
-// import 'react-toastify/scss/main.scss';
+import "react-toastify/scss/main.scss";
 import "../styles/globals.scss";
 
 import Layout from "components/Layout";
