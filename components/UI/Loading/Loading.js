706f8e81f4d7551aaf1a6da2d12d93ee8f32cc9c
Kevin Blair <kblair40@gmail.com>
2022-09-15 12:51:16 -0500

    Add custom svg for Loading

--- components/UI/Loading/Loading.js
+++ components/UI/Loading/Loading.js
@@ -1,37 +1,28 @@
 import React from "react";
 
-const Loading = ({ scale, stroke = "#fff" }) => {
+const Loading = () => {
   return (
-    <span
-      style={{
-        display: "block",
-        transform: `scale(${scale ? scale : 1})`,
-      }}
+    <svg
+      width="48"
+      height="48"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="animate-spin stroke-red-400"
     >
-      <svg
-        width="38"
-        height="38"
-        viewBox="0 0 38 38"
-        xmlns="http://www.w3.org/2000/svg"
-        stroke={stroke}
-      >
-        <g fill="none" fillRule="evenodd">
-          <g transform="translate(1 1)" strokeWidth="3">
-            <circle strokeOpacity=".5" cx="18" cy="18" r="18" />
-            <path d="M36 18c0-9.94-8.06-18-18-18">
-              <animateTransform
-                attributeName="transform"
-                type="rotate"
-                from="0 18 18"
-                to="360 18 18"
-                dur="1s"
-                repeatCount="indefinite"
-              />
-            </path>
-          </g>
-        </g>
-      </svg>
-    </span>
+      <circle
+        cx="24"
+        cy="24"
+        r="22"
+        fill="none"
+        stroke="black"
+        stroke-width="4"
+      />
+      <path
+        d="M36.9561 6.2197C39.7578 8.26126 42.0375 10.936 43.6091 14.0259C45.1808 17.1158 46 20.5334 46 24"
+        stroke-width="4"
+      />
+    </svg>
   );
 };
 
