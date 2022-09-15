c9c9af7f74d9bcc2893ca63bd618a18c526d9fb3
Kevin Blair <kblair40@gmail.com>
2022-09-15 14:32:12 -0500

    Refactor styles to tailwind / remove scss

--- components/GoBack/GoBack.js
+++ components/GoBack/GoBack.js
@@ -3,18 +3,17 @@ import Link from "next/link";
 import Image from "next/image";
 
 import backArrow from "public/assets/icons/arrow-left.svg";
-import styles from "./GoBack.module.scss";
 
 const GoBack = ({ label, route }) => {
   return (
     <Link href={route}>
-      <a className={styles.go_back}>
-        <div className={styles.go_back__icon}>
-          <Image src={backArrow} alt="Back arrow" />
+      <a className="flex items-center mt-2 mb-8 w-fit">
+        <div className="flex items-center">
+          <Image src={backArrow} alt="Back arrow" height="28px" width="28px" />
         </div>
 
-        <div className={styles.go_back__label_container}>
-          <p className={styles.go_back__label}>{label}</p>
+        <div className="ml-3 h-full">
+          <p className="whitespace-nowrap font-medium text-lg">{label}</p>
         </div>
       </a>
     </Link>
