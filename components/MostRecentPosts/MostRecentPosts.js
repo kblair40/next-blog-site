51290c9fb3ae0d6ca3005f25301bef87f896cba3
Kevin Blair <kblair40@gmail.com>
2022-09-17 06:53:13 -0500

    style improvements

--- components/MostRecentPosts/MostRecentPosts.js
+++ components/MostRecentPosts/MostRecentPosts.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import classNames from "classnames";
 
 import api from "utils/api";
+import Button from "components/UI/Button";
 import Loading from "components/UI/Loading";
 
 const MostRecentPosts = () => {
@@ -59,10 +60,25 @@ const RecentPost = ({ post }) => {
     <div>
       <div className="flex justify-between">
         <h2 className="text-lg font-medium">{post.title}</h2>
-        <div className="flex space-x-2 items-center border-b border-transparent duration-200 hover:border-slate-400 leading-none">
+
+        <Button
+          iconRight={
+            <svg
+              className="w-4 h-4 fill-slate-200 rotate-180 ml-2"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M11.3086 17.3867L10.4414 18.2539C10.0742 18.6211 9.48047 18.6211 9.11719 18.2539L1.52344 10.6641C1.15625 10.2969 1.15625 9.70313 1.52344 9.33984L9.11719 1.74609C9.48437 1.37891 10.0781 1.37891 10.4414 1.74609L11.3086 2.61328C11.6797 2.98438 11.6719 3.58984 11.293 3.95312L6.58594 8.4375H17.8125C18.332 8.4375 18.75 8.85547 18.75 9.375V10.625C18.75 11.1445 18.332 11.5625 17.8125 11.5625H6.58594L11.293 16.0469C11.6758 16.4102 11.6836 17.0156 11.3086 17.3867Z" />
+            </svg>
+          }
+        >
+          Read
+        </Button>
+
+        {/* <div className="flex space-x-2 items-center border-b border-transparent duration-200 hover:border-slate-400 leading-none">
           <button className="py-1 font-medium duration-200">Read</button>
           <p>-&gt;</p>
-        </div>
+        </div> */}
       </div>
 
       <div
--- components/UI/Button/Button.js
+++ components/UI/Button/Button.js
@@ -2,63 +2,129 @@ import React from "react";
 import classNames from "classnames";
 
 import Loading from "components/UI/Loading";
+import SolidButton from "./SolidButton";
 
 const Button = ({
   onClick,
   isDisabled,
   loading,
   children,
+  iconLeft,
+  iconRight,
   colorScheme = "primary",
+  variant = "solid",
   classes = [],
 }) => {
-  const colorClasses = {
-    slate: ["bg-slate-600", "hover:bg-slate-700", "active:bg-slate-800"],
-    emerald: [
-      "bg-emerald-600",
-      "hover:bg-emerald-700",
-      "active:bg-emerald-800",
-    ],
-    primary: [
-      "bg-primary-600",
-      "hover:bg-primary-700",
-      "active:bg-primary-800",
-    ],
-  };
-
-  const btnState = isDisabled ? "disabled" : "enabled";
-
-  const baseClasses = [
-    "duration-200",
-    "rounded-lg",
-    "px-3",
-    "h-10",
-    "text-white",
-    "font-medium",
-    "flex",
-    "items-center",
-    "justify-center",
-    ...colorClasses[colorScheme].map((color) => {
-      return isDisabled ? `${color}/50` : color;
-    }),
-    ...classes,
-  ];
-  // console.log("\n\nBASE CLASSES:", baseClasses);
-
-  return (
-    <button
-      onClick={onClick}
-      className={classNames(baseClasses)}
-      disabled={isDisabled}
-    >
-      {loading ? (
-        <div className="scale-50">
-          <Loading />
-        </div>
-      ) : (
-        children
-      )}
-    </button>
-  );
+  if (variant === "solid") {
+    return (
+      <SolidButton
+        onClick={onClick}
+        loading={loading}
+        iconLeft={iconLeft}
+        iconRight={iconRight}
+        isDisabled={isDisabled}
+        colorScheme={colorScheme}
+        classes={classes}
+      >
+        {children}
+      </SolidButton>
+    );
+  }
 };
 
 export default Button;
+
+// const SolidButton = () => {
+//   const colorClasses = {
+//     slate: ["bg-slate-600", "hover:bg-slate-700", "active:bg-slate-800"],
+//     emerald: [
+//       "bg-emerald-600",
+//       "hover:bg-emerald-700",
+//       "active:bg-emerald-800",
+//     ],
+//     primary: [
+//       "bg-primary-600",
+//       "hover:bg-primary-700",
+//       "active:bg-primary-800",
+//     ],
+//   };
+
+//   const baseClasses = [
+//     "duration-200",
+//     "rounded-lg",
+//     "px-3",
+//     "h-10",
+//     "text-white",
+//     "font-medium",
+//     "flex",
+//     "items-center",
+//     "justify-center",
+//     ...colorClasses[colorScheme].map((color) => {
+//       return isDisabled ? `${color}/50` : color;
+//     }),
+//     ...classes,
+//   ];
+
+//   return (
+//     <button
+//       onClick={onClick}
+//       className={classNames(baseClasses)}
+//       disabled={isDisabled}
+//     >
+//       {loading ? (
+//         <div className="scale-50">
+//           <Loading />
+//         </div>
+//       ) : (
+//         children
+//       )}
+//     </button>
+//   );
+// }
+
+//
+// const colorClasses = {
+//   slate: ["bg-slate-600", "hover:bg-slate-700", "active:bg-slate-800"],
+//   emerald: [
+//     "bg-emerald-600",
+//     "hover:bg-emerald-700",
+//     "active:bg-emerald-800",
+//   ],
+//   primary: [
+//     "bg-primary-600",
+//     "hover:bg-primary-700",
+//     "active:bg-primary-800",
+//   ],
+// };
+
+// const baseClasses = [
+//   "duration-200",
+//   "rounded-lg",
+//   "px-3",
+//   "h-10",
+//   "text-white",
+//   "font-medium",
+//   "flex",
+//   "items-center",
+//   "justify-center",
+//   ...colorClasses[colorScheme].map((color) => {
+//     return isDisabled ? `${color}/50` : color;
+//   }),
+//   ...classes,
+// ];
+
+// return (
+//   <button
+//     onClick={onClick}
+//     className={classNames(baseClasses)}
+//     disabled={isDisabled}
+//   >
+//     {loading ? (
+//       <div className="scale-50">
+//         <Loading />
+//       </div>
+//     ) : (
+//       children
+//     )}
+//   </button>
+// );
