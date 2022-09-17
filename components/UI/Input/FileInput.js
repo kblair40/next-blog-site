8dca99e5eeaf27ef385fa0c52d5e48af59da2508
Kevin Blair <kblair40@gmail.com>
2022-09-17 09:57:27 -0500

    Clean up unused code

--- components/UI/Input/FileInput.js
+++ components/UI/Input/FileInput.js
@@ -4,28 +4,16 @@ import Script from "next/script";
 import Button from "components/UI/Button";
 
 const CLOUD_NAME = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
-const API_KEY = process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY;
-const API_SECRET = process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET;
-
-const UPLOAD_URL = () => `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/upload`;
+// const API_KEY = process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY;
+// const API_SECRET = process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET;
 
 const FileInput = ({ onChange }) => {
-  const inputRef = useRef();
-
-  const handleClick = () => {
-    inputRef.current.click();
-  };
-
-  const handleChange = (e) => {
-    console.log("E.TARGET:", e.target.files);
-    onChange();
-  };
-
   const uploader = useRef();
+
   useEffect(() => {
     uploader.current = cloudinary.createUploadWidget(
       {
-        cloudName: "erinsblog",
+        cloudName: CLOUD_NAME,
         uploadPreset: "zdniolnp",
       },
       (error, result) => {
@@ -36,17 +24,10 @@ const FileInput = ({ onChange }) => {
     );
   }, []);
 
-  // uploadFile('https://res.cloudinary.com/demo/image/upload/sample.jpg')
-
   return (
     <div className="block">
-      <Script
-        src="https://upload-widget.cloudinary.com/global/all.js"
-        // type="text/javascript"
-        // strategy="beforeInteractive"
-      />
+      <Script src="https://upload-widget.cloudinary.com/global/all.js" />
 
-      {/* <Button onClick={handleClick} classes={["ml-4"]}> */}
       <Button
         onClick={() => {
           if (uploader.current) {
@@ -58,13 +39,13 @@ const FileInput = ({ onChange }) => {
         Upload Image
       </Button>
 
-      <input
+      {/* <input
         ref={inputRef}
         type="file"
         accept="image/jpg image/jpeg image/png image/webp"
         onChange={handleChange}
         hidden
-      />
+      /> */}
     </div>
   );
 };
