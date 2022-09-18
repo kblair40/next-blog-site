bdda50763e0a7c5456ae54309eb486d5d77f1b35
Kevin Blair <kblair40@gmail.com>
2022-09-18 16:00:49 -0500

    Add padding as optional class

--- components/UI/IconButton/IconButton.js
+++ components/UI/IconButton/IconButton.js
@@ -1,17 +1,14 @@
 import React from "react";
 import classNames from "classnames";
 
-const IconButton = ({ onClick, icon }) => {
-  // const baseClasses = [
-  //   "text-white text-sm text-center bg-blue-700 rounded-lg p-2.5 mr-2",
-  //   "inline-flex items-center",
-  //   "hover:bg-blue-800 focus-visible:outline-none",
-  // ];
+const IconButton = ({ onClick, icon, classes = [], padding = "p-2.5" }) => {
   const baseClasses = [
-    "text-black text-sm text-center bg-transparent rounded-lg p-2.5 mr-2",
+    "text-black text-sm text-center bg-transparent rounded-lg mr-2",
     "inline-flex items-center",
     "hover:bg-slate-200 focus-visible:outline-none duration-300",
   ];
+
+  baseClasses = [...baseClasses, ...classes, padding];
 
   return (
     <button type="button" onClick={onClick} className={classNames(baseClasses)}>
