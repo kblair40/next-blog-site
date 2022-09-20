ed72b6c6f3411f81176673942d1d0de9c162b40e
Kevin Blair <kblair40@gmail.com>
2022-09-20 13:59:56 -0500

    Finish quick start with google docs

--- components/Admin/CreatePost/CreatePost.js
+++ components/Admin/CreatePost/CreatePost.js
@@ -5,6 +5,7 @@ import LocalInput from "components/Admin/LocalInput";
 import Input from "components/UI/Input";
 import Button from "components/UI/Button";
 import FileInput from "components/Admin/FileInput";
+import GoogleTest from "components/Admin/GoogleTest";
 
 const CreatePost = () => {
   const [postTitle, setPostTitle] = useState("");
@@ -17,45 +18,53 @@ const CreatePost = () => {
   ]);
 
   return (
-    <div className="flex justify-center">
-      <div className="flex flex-col space-y-4 items-center w-80">
-        <div className="flex items-center space-x-2 w-full">
-          <p className={labelClasses}>Title:</p>
-          <Input onChange={(val) => setPostTitle(val)} />
-        </div>
-
-        <div className="flex items-center space-x-2 w-full">
-          <p className={labelClasses}>Image URL:</p>
-          <Input onChange={(val) => setPostImageUrl(val)} />
-        </div>
-
-        <div className="w-80">
+    <React.Fragment>
+      <div className="flex justify-center">
+        <div className="flex flex-col space-y-4 items-center w-80">
           <div className="flex items-center space-x-2 w-full">
-            <p className={labelClasses}>Status:</p>
-            <LocalInput
-              defaultValue={2}
-              type="number"
-              onChange={(val) => setStatus(val)}
-            />
+            <p className={labelClasses}>Title:</p>
+            <Input onChange={(val) => setPostTitle(val)} />
           </div>
 
-          <div className="flex justify-end mt-1 space-x-4 text-sm text-slate-600">
-            <p>1 = show immediately</p>
-            <p>2 = hidden</p>
+          <div className="flex items-center space-x-2 w-full">
+            <p className={labelClasses}>Image URL:</p>
+            <Input onChange={(val) => setPostImageUrl(val)} />
           </div>
-        </div>
 
-        <div>
-          <FileInput onChange={(file) => setPostFile(file)} />
-          {postFile ? (
-            <div className="flex space-x-2 mt-1">
-              <p className="font-medium">File Name:</p>
-              <p>{postFile}</p>
+          <div className="w-80">
+            <div className="flex items-center space-x-2 w-full">
+              <p className={labelClasses}>Status:</p>
+              <LocalInput
+                defaultValue={2}
+                type="number"
+                onChange={(val) => setStatus(val)}
+              />
             </div>
-          ) : null}
+
+            <div className="flex justify-end mt-1 space-x-4 text-sm text-slate-600">
+              <p>1 = show immediately</p>
+              <p>2 = hidden</p>
+            </div>
+          </div>
+
+          <div>
+            <FileInput onChange={(file) => setPostFile(file)} />
+            {postFile ? (
+              <div className="flex space-x-2 mt-1">
+                <p className="font-medium">File Name:</p>
+                <p>{postFile}</p>
+              </div>
+            ) : null}
+          </div>
         </div>
       </div>
-    </div>
+
+      <div className="mt-12 flex justify-center">
+        <div className="max-w-3xl">
+          <GoogleTest />
+        </div>
+      </div>
+    </React.Fragment>
   );
 };
 
