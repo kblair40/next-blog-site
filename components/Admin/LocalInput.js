89055e37585494ea543e7275894043db2f7e923a
Kevin Blair <kblair40@gmail.com>
2022-09-20 06:16:31 -0500

    Add numeric variant

--- components/Admin/LocalInput.js
+++ components/Admin/LocalInput.js
@@ -2,16 +2,29 @@ import React from "react";
 import classNames from "classnames";
 
 const LocalInput = ({ defaultValue, onChange, type = "text" }) => {
-  const inputClasses = ["rounded-md h-10 border border-slate-200 px-2 text-sm"];
-
-  return (
-    <input
-      type={type}
-      className={classNames(inputClasses)}
-      defaultValue={defaultValue}
-      onChange={onChange}
-    />
-  );
+  const inputClasses = ["rounded-md h-8 border border-slate-200 px-2 text-sm"];
+  if (type === "text") {
+    return (
+      <input
+        type="text"
+        className={classNames(inputClasses)}
+        defaultValue={defaultValue}
+        onChange={onChange}
+      />
+    );
+  } else if (type === "number") {
+    return (
+      <input
+        type="number"
+        className={classNames(inputClasses)}
+        defaultValue={defaultValue}
+        onChange={onChange}
+        min="1"
+        max="3"
+        step="1"
+      />
+    );
+  }
 };
 
 export default LocalInput;
