76ad2a3152c1075ce0d9756c931efd2b256222ed
Kevin Blair <kblair40@gmail.com>
2022-09-21 08:16:57 -0500

    Add skeleton for statuses component

---
+++ components/Admin/Statuses/Statuses.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const Statuses = ({ statusOptions }) => {
+  const [statuses, setStatuses] = useState(statusOptions);
+
+  return <div>Statuses</div>;
+};
+
+export default Statuses;
