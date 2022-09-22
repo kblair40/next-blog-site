82271c23da80113bd908487616f4431c57febe71
Kevin Blair <kblair40@gmail.com>
2022-09-22 06:24:50 -0500

    Cleanup console.logs

--- pages/post-render-test/PostRenderTest.js
+++ pages/post-render-test/PostRenderTest.js
@@ -46,11 +46,7 @@ const PostRenderTest = () => {
     if (type === "div") el.classes += " border border-slate-200/50";
 
     const classes = { className: classNames(el.classes.split(" ")), key: i };
-    console.log("EL:", el, { type, classes });
 
-    if (type === "div") {
-      console.log("\n\n\nFOUND DIV\n\n\n");
-    }
     const newElement = createElement(el.el.value, classes, innerText);
     return newElement;
   };
