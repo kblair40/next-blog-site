e8480959c10ddafe63939f916126497170f8b8c4
Kevin Blair <kblair40@gmail.com>
2022-09-22 06:42:19 -0500

    Finish default props for carousel

--- components/FeaturedPosts/Carousel/CarouselNew.js
+++ components/FeaturedPosts/Carousel/CarouselNew.js
@@ -6,11 +6,12 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 const CarouselNew = ({ postsArray }) => {
   const [endReached, setEndReached] = useState(false);
 
-  const localCarouselRef = useRef();
+  // const localCarouselRef = useRef();
 
   const classes = classNames({
     "w-full h-fit": true,
-    "sm:pl-[5vw] md:pl-[10vw] carousel w-screen border-transparent relative": true,
+    "w-screen border border-slate-400 relative": true,
+    // "sm:pl-[5vw] md:pl-[10vw] carousel w-screen border-transparent relative": true,
     "custom-cursor": !endReached,
   });
   return (
@@ -24,10 +25,25 @@ const CarouselNew = ({ postsArray }) => {
       }}
     >
       {postsArray && postsArray.length ? (
-        <Carousel>{postsArray}</Carousel>
+        <Carousel
+          centerMode={true}
+          showStatus={false}
+          showIndicators={false}
+          showThumbs={false}
+          // showArrows={false}
+        >
+          {postsArray}
+        </Carousel>
       ) : null}
     </div>
   );
 };
 
 export default CarouselNew;
+
+// ORIGINAL
+// const classes = classNames({
+//   "w-full h-fit": true,
+//   "sm:pl-[5vw] md:pl-[10vw] carousel w-screen border-transparent relative": true,
+//   "custom-cursor": !endReached,
+// });
