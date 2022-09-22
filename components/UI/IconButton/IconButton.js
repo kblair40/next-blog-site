f2ae9ce06f453d6033813a38df874197a3854ff4
Kevin Blair <kblair40@gmail.com>
2022-09-22 10:13:47 -0500

    Add disabled prop and disabled styles

--- components/UI/IconButton/IconButton.js
+++ components/UI/IconButton/IconButton.js
@@ -1,17 +1,29 @@
 import React from "react";
 import classNames from "classnames";
 
-const IconButton = ({ onClick, icon, classes = [], padding = "p-2.5" }) => {
+const IconButton = ({
+  onClick,
+  icon,
+  disabled,
+  classes = [],
+  padding = "p-2.5",
+}) => {
   let baseClasses = [
     "text-black text-sm text-center bg-transparent rounded-lg mr-2",
     "inline-flex items-center",
     "hover:bg-slate-200 focus-visible:outline-none duration-300",
+    "disabled:hover:bg-transparent disabled:opacity-50",
   ];
 
   baseClasses = [...baseClasses, ...classes, padding];
 
   return (
-    <button type="button" onClick={onClick} className={classNames(baseClasses)}>
+    <button
+      type="button"
+      onClick={onClick}
+      className={classNames(baseClasses)}
+      disabled={disabled}
+    >
       {icon}
       <span className="sr-only">Icon description</span>
     </button>
