f9f9e6dd71fc1dc3fd607a610ee8ec54ec8bfc5a
Kevin Blair <kblair40@gmail.com>
2022-09-24 09:53:31 -0500

    Finish category styles

--- pages/posts/Categories.js
+++ pages/posts/Categories.js
@@ -11,7 +11,7 @@ const Categories = () => {
     "grid justify-center": true,
     "grid-cols-2 md:grid-cols-3": true,
     "grid-rows-3 md:grid-rows-2": true,
-    "gap-x-4 sm:gap-x-8 md:gap-x-10 gap-y-6 md:gap-y-8": true,
+    "gap-x-2 sm:gap-x-4 lg:gap-x-6 gap-y-6 md:gap-y-8": true,
   });
 
   return (
@@ -29,19 +29,20 @@ const Category = ({ category: { label, img } }) => {
   const categoryClasses = classNames({
     "flex flex-col items-center": true,
     "space-y-3": true,
-    "p-1": true,
+    "p-2 md:p-3 rounded-lg": true,
+    "cursor-pointer duration-500 hover:bg-pink": true,
   });
 
   return (
     <div className={categoryClasses}>
       <Image
-        className="rounded-full w-36 h-36 sm:h-48 sm:w-48 md:w-52 md:h-52 lg:h-60 lg:w-60" // 192 x 192
+        className="rounded-full w-36 h-36 sm:h-48 sm:w-48 lg:h-60 lg:w-60" // 192 x 192
         alt="category img"
         src={img}
         width={200}
         height={200}
       />
-      <p className="text-center font-heading font-medium tracking-wide text-lg sm:text-xl">
+      <p className="text-center font-heading font-medium leading-snug tracking-wide text-lg sm:text-xl">
         {label}
       </p>
     </div>
--- pages/posts/PostsPage.js
+++ pages/posts/PostsPage.js
@@ -6,8 +6,6 @@ import Categories from "./Categories";
 const PostsPage = () => {
   return (
     <PostsPageWrapper>
-      <div>PostsPage</div>
-
       <Categories />
     </PostsPageWrapper>
   );
