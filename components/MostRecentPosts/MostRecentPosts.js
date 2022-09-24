0f8136231cc727bfd75b79f213edc8ec185b39a1
Kevin Blair <kblair40@gmail.com>
2022-09-24 07:09:27 -0500

    Update headings on home page to use font-heading

--- components/MostRecentPosts/MostRecentPosts.js
+++ components/MostRecentPosts/MostRecentPosts.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 import api from "utils/api";
-import Button from "components/UI/Button";
+// import Button from "components/UI/Button";
 import Loading from "components/UI/Loading";
 
 const MostRecentPosts = () => {
@@ -36,9 +36,9 @@ const MostRecentPosts = () => {
 
   return (
     <div className="">
-      <h2 className="text-2xl text-center font-semibold border-b border-slate-300">
+      <h3 className="text-2xl text-center font-medium border-b border-slate-300">
         Recent Posts
-      </h2>
+      </h3>
 
       <div className="flex flex-col">
         {recentPosts.length
@@ -55,25 +55,20 @@ export default MostRecentPosts;
 
 const RecentPost = ({ post }) => {
   return (
-    <div className="py-2 px-1 cursor-pointer duration-200 hover:bg-darkgreen/5 active:bg-darkgreen/10">
+    <div className="py-2 px-2 cursor-pointer duration-200 hover:bg-lightgreen/10 active:bg-lightgreen/20">
       <div className="flex items-center justify-between">
-        <h2 className="text-lg font-medium">{post.title}</h2>
+        <h4 className="text-lg font-medium">{post.title}</h4>
 
-        <Button
-          size="sm"
-          variant="ghost"
-          iconRight={
-            <svg
-              className="w-3 h-3 fill-slate-800 rotate-180 ml-2"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M11.3086 17.3867L10.4414 18.2539C10.0742 18.6211 9.48047 18.6211 9.11719 18.2539L1.52344 10.6641C1.15625 10.2969 1.15625 9.70313 1.52344 9.33984L9.11719 1.74609C9.48437 1.37891 10.0781 1.37891 10.4414 1.74609L11.3086 2.61328C11.6797 2.98438 11.6719 3.58984 11.293 3.95312L6.58594 8.4375H17.8125C18.332 8.4375 18.75 8.85547 18.75 9.375V10.625C18.75 11.1445 18.332 11.5625 17.8125 11.5625H6.58594L11.293 16.0469C11.6758 16.4102 11.6836 17.0156 11.3086 17.3867Z" />
-            </svg>
-          }
-        >
-          Read
-        </Button>
+        <div className="flex items-center space-x-2 group">
+          {/* <p className="group-hover:decoration-solid">Read</p> */}
+          <svg
+            className="w-3 h-3 fill-slate-800 rotate-180 ml-2"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M11.3086 17.3867L10.4414 18.2539C10.0742 18.6211 9.48047 18.6211 9.11719 18.2539L1.52344 10.6641C1.15625 10.2969 1.15625 9.70313 1.52344 9.33984L9.11719 1.74609C9.48437 1.37891 10.0781 1.37891 10.4414 1.74609L11.3086 2.61328C11.6797 2.98438 11.6719 3.58984 11.293 3.95312L6.58594 8.4375H17.8125C18.332 8.4375 18.75 8.85547 18.75 9.375V10.625C18.75 11.1445 18.332 11.5625 17.8125 11.5625H6.58594L11.293 16.0469C11.6758 16.4102 11.6836 17.0156 11.3086 17.3867Z" />
+          </svg>
+        </div>
       </div>
 
       <div
--- components/ResourcesImLoving/ResourcesImLoving.js
+++ components/ResourcesImLoving/ResourcesImLoving.js
@@ -6,9 +6,9 @@ import Resources from "./Resources";
 const ResourcesImLoving = () => {
   return (
     <div className="w-full">
-      <h2 className="text-2xl text-center font-semibold border-b border-slate-300">
+      <h3 className="text-2xl text-center font-medium border-b border-slate-300">
         Resources I'm Loving
-      </h2>
+      </h3>
 
       <Resources />
     </div>
