088a22b1012ee3b0ca80da050e44fa47d297461b
Kevin Blair <kblair40@gmail.com>
2022-09-25 14:50:24 -0500

    add skeletons for about and resources pages

---
+++ pages/about/AboutPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      About Page
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ pages/resources/ResourcesPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ResourcesPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen">
+      Resources Page
+    </div>
+  );
+};
+
+export default ResourcesPage;
