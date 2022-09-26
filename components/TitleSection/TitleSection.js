82f345d24204594ffb6136b80f30295b9378891b
Kevin Blair <kblair40@gmail.com>
2022-09-26 07:42:28 -0500

    Fix TitleSection width changing depending on which category is actively selected

--- components/TitleSection/TitleSection.js
+++ components/TitleSection/TitleSection.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const TitleSection = ({ sectionTitle }) => {
   return (
-    <div className="h-full max-w-[140px] text-slate-800 flex flex-col items-center justify-center">
-      <h1 className="text-5xl -rotate-90 tracking-widest text-center font-light tracking-wide leading-snug">
+    <div className="h-full w-full max-w-[140px] text-slate-800 flex flex-col items-center justify-center">
+      <h1 className="text-5xl -rotate-90 tracking-widest text-center font-light leading-snug">
         {sectionTitle ? sectionTitle.toUpperCase() : ""}
       </h1>
     </div>
