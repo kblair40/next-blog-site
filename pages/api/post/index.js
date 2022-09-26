9c0b1ff312d18fe8b06a2eb6085fe42c48d438dc
Kevin Blair <kblair40@gmail.com>
2022-09-26 06:38:03 -0500

    Add skeleton for get request and buildQuery function

---
+++ pages/api/post/index.js
@@ -0,0 +1,26 @@
+import dbConnect from "utils/dbConnect";
+import Post from "server/models/Post";
+
+const buildQuery = (query) => {
+  console.log("BUILD QUERY RCVD:", query, typeof query);
+};
+
+export default async function handler(req, res) {
+  const { method, query } = req;
+
+  try {
+    await dbConnect();
+  } catch (err) {
+    console.log("FAILED CONNECTING TO MONGO:", err);
+    return;
+  }
+
+  switch (method) {
+    case "GET":
+      let filter;
+      if (query) {
+        filter = buildQuery(query);
+      }
+      return res.status(400).json({ success: false, msg: "UNHANDLED ERROR" });
+  }
+}
