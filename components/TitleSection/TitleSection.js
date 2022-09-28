49a6e8d18426e85471156392f800857dc3436598
Kevin Blair <kblair40@gmail.com>
2022-09-28 11:34:41 -0500

    Add logo to top of TitleSection

--- components/TitleSection/TitleSection.js
+++ components/TitleSection/TitleSection.js
@@ -1,13 +1,40 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import classNames from "classnames";
+
+import logo from "public/assets/images/logo_sm.png";
 
 const TitleSection = ({ sectionTitle }) => {
+  const titleWrapperClasses = classNames({
+    "hidden sm:flex sm:flex-col sm:items-center sm:justify-center": true,
+    "h-full w-full sm:max-w-80px md:max-w-140px": true,
+    "text-slate-800 relative": true,
+  });
+
+  const titleClasses = classNames([
+    "text-2xl sm:text-3xl md:text-5xl",
+    "-rotate-90 tracking-widest leading-snug",
+    "font-light text-center",
+  ]);
+
   return (
-    <div className="hidden h-full w-full sm:max-w-[80px] md:max-w-[140px] text-slate-800 sm:flex sm:flex-col sm:items-center sm:justify-center">
-      <h1 className="text-2xl sm:text-3xl md:text-5xl -rotate-90 tracking-widest text-center font-light leading-snug">
+    <div className={titleWrapperClasses}>
+      <h1 className={titleClasses}>
         {sectionTitle ? sectionTitle.toUpperCase() : ""}
       </h1>
+
+      <div className="absolute top-4 z-50">
+        <Link href="/">
+          <a>
+            <Image src={logo} width={140} height={66} />
+          </a>
+        </Link>
+      </div>
     </div>
   );
 };
 
 export default TitleSection;
+
+// 0.470967741935
