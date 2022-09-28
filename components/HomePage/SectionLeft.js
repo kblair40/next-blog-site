55118a7dfad9da066887b423714347b920b99690
Kevin Blair <kblair40@gmail.com>
2022-09-28 11:14:04 -0500

    increase font size for lg+ screens

--- components/HomePage/SectionLeft.js
+++ components/HomePage/SectionLeft.js
@@ -3,7 +3,7 @@ import React from "react";
 const SectionLeft = () => {
   return (
     <div className="h-full bg-darkgreen text-white flex flex-col items-center justify-center px-4">
-      <h1 className="text-4xl sm:text-5xl text-center font-medium tracking-wide leading-snug">
+      <h1 className="text-4xl sm:text-5xl lg:text-6xl text-center font-medium tracking-wide leading-snug">
         MONEY AND OTHER THINGS
       </h1>
       <h6 className="text-center text-sm font-normal mt-4 tracking-widest">
