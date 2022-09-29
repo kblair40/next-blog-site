8062598165456df2d5cea208f3eb62340c75bf48
Kevin Blair <kblair40@gmail.com>
2022-09-29 17:49:38 -0500

    Add calculator to resources page

--- pages/resources/ResourcesPage.js
+++ pages/resources/ResourcesPage.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Link from "next/link";
+
+import TimeCurrencyCalculator from "components/Resources/TimeCurrencyCalculator";
 
 const ResourcesPage = () => {
   return (
     <div className="flex justify-center items-center h-screen w-screen">
-      Resources Page
+      <div className="max-w-md sm:max-w-2xl px-6">
+        <TimeCurrencyCalculator />
+      </div>
     </div>
   );
 };
