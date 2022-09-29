cfc172d54666af5863a2bfc7904f8a9288d8739c
Kevin Blair <kblair40@gmail.com>
2022-09-29 18:06:49 -0500

    Add Resources Page

--- pages/resources/ResourcesPage.js
+++ pages/resources/ResourcesPage.js
@@ -1,16 +1,30 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
+import logo from "public/assets/images/logo_sm.png";
 import TimeCurrencyCalculator from "components/Resources/TimeCurrencyCalculator";
 
 const ResourcesPage = () => {
   return (
-    <div className="flex justify-center items-center h-screen w-screen">
-      <div className="max-w-md sm:max-w-2xl px-6">
+    <div className="flex justify-center items-center h-screen w-screen py-4">
+      <div className="min-w-[320px] max-w-md sm:max-w-2xl px-6">
         <TimeCurrencyCalculator />
       </div>
+
+      <HomeLink />
     </div>
   );
 };
 
 export default ResourcesPage;
+
+const HomeLink = () => {
+  return (
+    <Link href="/">
+      <a className="absolute top-2 left-4">
+        <Image alt="logo" src={logo} width={140} height={66} />
+      </a>
+    </Link>
+  );
+};
