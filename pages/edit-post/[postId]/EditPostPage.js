091337ed350865c6ec554007c972d750e4edc630
Kevin Blair <kblair40@gmail.com>
2022-10-07 09:32:35 -0500

    Add skeleton and initial api call for edit post page

---
+++ pages/edit-post/[postId]/EditPostPage.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const EditPostPage = ({ content, postId }) => {
+  return <div>EditPostPage</div>;
+};
+
+export default EditPostPage;
---
+++ pages/edit-post/[postId]/index.js
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+import api from "utils/api";
+import Button from "components/UI/Button";
+import EditPostPage from "./EditPostPage";
+
+const Wrapper = () => {
+  const [content, setContent] = useState();
+
+  const { query } = useRouter();
+
+  console.log("QUERY:", query);
+
+  useEffect(() => {
+    const fetchPost = async (postId) => {
+      try {
+        const response = await api.get("/posts", {
+          params: { id: postId },
+        });
+
+        console.log("\nRESPONSE:", response);
+
+        if (response.data && response.data.post) {
+          setContent(response.data.post);
+        }
+      } catch (e) {
+        console.log("FAILED FETCHING POST:", e);
+      }
+    };
+
+    if (query && query.postId) {
+      fetchPost(query.postId);
+    }
+  }, [query]);
+
+  if (!content) {
+    return (
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        Loading
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div className="fixed bottom-2 left-2">
+        <Button size="sm">Save Changes</Button>
+      </div>
+      <EditPostPage content={content} />
+    </div>
+  );
+};
+
+export default Wrapper;
