e9ef0d30534902170e5ed8f21781a642cd75ec53
Kevin Blair <kblair40@gmail.com>
2022-10-16 18:40:55 -0500

    Add final icons

--- pages/home-with-banner/TestPage.js
+++ pages/home-with-banner/TestPage.js
@@ -1,34 +1,63 @@
 import React from "react";
 
-import banner_nav from "public/assets/images/banner-nav.svg";
-
-// Banner left 30px, width 94px
-// position: absolute;
-// bottom: -3.75rem;
-// left: 30px;
-// width: 94px;
-// margin-bottom: 3.75rem;
-// padding: 0.5rem 0.5rem 0;
-// border: 1px solid #000;
-// border-top: none;
-// background: #fff;
-// filter: drop-shadow(3px 10px 4px rgba(0,0,0,.2));
-// transform: translateY(100%);
-
 const TestPage = () => {
   return (
-    <div className="relative flex flex-col items-center border border-red-200 h-100 w-24">
-      <div className="absolute left-7 border border-green-300 h-full bottom-4">
+    <div className="relative left-7 flex flex-col items-center h-100 w-24">
+      <div className="absolute">
         <BannerNav />
       </div>
 
-      <HeartIcon />
-      <ShirtIcon />
+      <div className="absolute flex flex-col items-center space-y-6 mt-4">
+        <NavButton label="Style">
+          <ShirtIcon />
+        </NavButton>
+
+        <NavButton label="Love">
+          <HeartIcon />
+        </NavButton>
+
+        <NavButton label="Travel" isActive={true}>
+          <PlaneIcon />
+        </NavButton>
+
+        <NavButton label="Hungry">
+          <FoodIcon />
+        </NavButton>
+      </div>
     </div>
   );
 };
 
 export default TestPage;
+
+const NavButton = ({ label, isActive, children }) => {
+  return (
+    <div className="flex flex-col items-center font-semibold relative">
+      {children}
+      <p className={label === "Hungry" ? "pt-2" : undefined}>{label}</p>
+
+      {isActive && (
+        <div className="flex justify-between absolute z-20 w-full h-full border-l-2 border-r-2 border-darkgreen" />
+      )}
+    </div>
+  );
+};
+
+// const BannerNav = () => {
+//   return (
+//     <svg
+//       className="z-0"
+//       width="94"
+//       height="527"
+//       viewBox="0 0 94 527"
+//       fill="none"
+//       xmlns="http://www.w3.org/2000/svg"
+//     >
+//       <rect width="94" height="500" fill="#f5f5f5" />
+//       <path d="M47 500L0 526.015V500H94V526.015L47 500Z" fill="#f5f5f5" />
+//     </svg>
+//   );
+// };
 
 const BannerNav = () => {
   return (
@@ -46,10 +75,10 @@ const BannerNav = () => {
   );
 };
 
-const ShirtIcon = () => {
+const ShirtIcon = ({ fill }) => {
   return (
     <svg
-      className="z-10"
+      className="z-10 scale-75"
       width="94"
       height="94"
       viewBox="0 0 94 94"
@@ -67,10 +96,10 @@ const ShirtIcon = () => {
   );
 };
 
-const HeartIcon = () => {
+const HeartIcon = ({ fill }) => {
   return (
     <svg
-      className="z-10"
+      className="z-10 scale-75"
       width="94"
       height="94"
       viewBox="0 0 94 94"
@@ -80,6 +109,27 @@ const HeartIcon = () => {
       <path
         d="M76.3751 53.1572L47.0001 82.2502L17.6251 53.1572M17.6251 53.1572C15.6876 51.2717 14.1614 49.0056 13.1427 46.5013C12.124 43.9971 11.6348 41.3091 11.706 38.6065C11.7772 35.9039 12.4072 33.2454 13.5563 30.7982C14.7054 28.3511 16.3488 26.1684 18.3829 24.3876C20.4171 22.6068 22.7979 21.2664 25.3755 20.451C27.953 19.6355 30.6716 19.3625 33.3598 19.6493C36.0481 19.9361 38.6478 20.7763 40.9954 22.1172C43.3429 23.4581 45.3874 25.2705 47.0001 27.4403C48.6198 25.2863 50.6667 23.4897 53.0126 22.163C55.3585 20.8364 57.9531 20.0083 60.6338 19.7305C63.3145 19.4526 66.0237 19.7311 68.5918 20.5485C71.1599 21.3659 73.5317 22.7046 75.5587 24.4808C77.5857 26.2569 79.2242 28.4324 80.3718 30.8709C81.5193 33.3095 82.1512 35.9586 82.2278 38.6526C82.3045 41.3466 81.8242 44.0274 80.8171 46.5272C79.8101 49.0271 78.2978 51.2921 76.3751 53.1807"
         stroke="black"
+        strokeWidth="9.26231"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+const PlaneIcon = ({ fill }) => {
+  return (
+    <svg
+      className="z-10 scale-75"
+      width="94"
+      height="94"
+      viewBox="0 0 94 94"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M62.6666 39.1667H78.3333C80.4108 39.1667 82.4032 39.992 83.8723 41.461C85.3413 42.93 86.1666 44.9225 86.1666 47C86.1666 49.0775 85.3413 51.07 83.8723 52.539C82.4032 54.008 80.4108 54.8333 78.3333 54.8333H62.6666L46.9999 82.25H35.2499L43.0833 54.8333H27.4166L19.5833 62.6667H7.83325L15.6666 47L7.83325 31.3333H19.5833L27.4166 39.1667H43.0833L35.2499 11.75H46.9999L62.6666 39.1667Z"
+        stroke="black"
         stroke-width="9.26231"
         stroke-linecap="round"
         stroke-linejoin="round"
@@ -87,17 +137,25 @@ const HeartIcon = () => {
     </svg>
   );
 };
-// const BannerNav = () => {
-//   return (
-//     <svg
-//       width="94"
-//       height="627"
-//       viewBox="0 0 94 627"
-//       fill="none"
-//       xmlns="http://www.w3.org/2000/svg"
-//     >
-//       <rect width="94" height="600" fill="#eee" />
-//       <path d="M47 600L0 626.015V600H94V626.015L47 600Z" fill="#eee" />
-//     </svg>
-//   );
-// };
+
+const FoodIcon = ({ fill }) => {
+  return (
+    <svg
+      className="z-10 scale-90"
+      width="68"
+      height="81"
+      viewBox="0 0 68 81"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M29.536 23.1778H20.7016V2.56433H14.812V23.1778H5.9777V2.56433H0.0881348V25.3864C0.0881348 33.7615 6.46635 40.7348 14.812 42.0824V80.6011H20.7016V42.0824C29.0473 40.7348 35.4255 33.7615 35.4255 25.3864V2.56433H29.536V23.1778ZM17.7568 36.4293C12.6382 36.4293 8.27389 33.3517 6.65316 29.0674H28.8605C27.2398 33.3517 22.8754 36.4293 17.7568 36.4293Z"
+        fill="black"
+      />
+      <path
+        d="M64.4257 1.12633C58.4011 2.05313 52.9073 5.10584 48.9383 9.73206C44.9694 14.3583 42.7876 20.2524 42.7876 26.3478V57.0428H61.9287V80.601H67.8183V0.60437L64.4257 1.12633ZM61.9287 51.1532H48.6772V26.3478C48.657 22.253 49.9261 18.2556 52.3045 14.9222C54.6829 11.5887 58.05 9.08834 61.9287 7.77528V51.1532Z"
+        fill="black"
+      />
+    </svg>
+  );
+};
