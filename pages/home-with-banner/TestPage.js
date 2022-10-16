bebfa389d9c03315bad387e7b4cc429e93f0d006
Kevin Blair <kblair40@gmail.com>
2022-10-16 11:10:30 -0500

    Add separate component to render banner icon

--- pages/home-with-banner/TestPage.js
+++ pages/home-with-banner/TestPage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 
 import banner_nav from "public/assets/images/banner-nav.svg";
 
@@ -18,32 +17,39 @@ import banner_nav from "public/assets/images/banner-nav.svg";
 
 const TestPage = () => {
   return (
-    <div className="absolute left-7">
-      <Image
-        alt="banner-navigation"
-        src={banner_nav}
-        height="600px"
-        width="94px"
-      />
+    <div className="absolute left-7 border ">
+      <BannerNav />
     </div>
   );
 };
 
 export default TestPage;
 
-const icons = {
-  banner: () => {},
+const BannerNav = () => {
+  return (
+    <svg
+      width="94"
+      height="627"
+      viewBox="0 0 94 627"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="94" height="600" fill="#eee" />
+      <path d="M47 600L0 626.015V600H94V626.015L47 600Z" fill="#eee" />
+    </svg>
+  );
 };
-
-const bannerIcon = (
-  <svg
-    width="94"
-    height="649"
-    viewBox="0 0 94 649"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <rect x="0.5" y="0.5" width="93" height="599" stroke="black" />
-    <path d="M92.8276 600.5L47 648.278L1.17242 600.5H92.8276Z" stroke="black" />
-  </svg>
-);
+// const BannerNav = () => {
+//   return (
+//     <svg
+//       width="94"
+//       height="627"
+//       viewBox="0 0 94 627"
+//       fill="none"
+//       xmlns="http://www.w3.org/2000/svg"
+//     >
+//       <rect width="94" height="600" fill="#eee" />
+//       <path d="M47 600L0 626.015V600H94V626.015L47 600Z" fill="#eee" />
+//     </svg>
+//   );
+// };
