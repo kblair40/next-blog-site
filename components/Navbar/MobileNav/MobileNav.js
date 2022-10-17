5d5a26053058d661fdd0fc740b91d9d49c754990
Kevin Blair <kblair40@gmail.com>
2022-10-17 09:32:39 -0500

    Add MobileNav component

---
+++ components/Navbar/MobileNav/MobileNav.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+
+import HamburgerButton from "../HamburgerButton";
+import Drawer from "../Drawer";
+import DrawerOverlay from "../DrawerOverlay";
+
+const MobileNav = () => {
+  const [subscribeModalOpen, setSubscribeModalOpen] = useState(false);
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const { query, pathname } = useRouter();
+  console.log("\n\nNAV QUERY:", query, "\n\n");
+
+  return (
+    <React.Fragment>
+      <div className="bg-white z-20 md:hidden">
+        <Drawer
+          pathname={pathname}
+          isOpen={drawerOpen}
+          onClose={() => setDrawerOpen(false)}
+        />
+        <DrawerOverlay
+          isOpen={drawerOpen}
+          onClose={() => setDrawerOpen((prev) => !prev)}
+        />
+      </div>
+
+      <HamburgerButton
+        isOpen={drawerOpen}
+        onClick={() => setDrawerOpen((prev) => !prev)}
+      />
+    </React.Fragment>
+  );
+};
+
+export default MobileNav;
