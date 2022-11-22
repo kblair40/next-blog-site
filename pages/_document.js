6c55638f206ec1cc57a28c45deea9a385e113f92
Kevin Blair <kblair40@gmail.com>
2022-11-22 06:31:58 -0600

    replace fonts

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ const Document = () => {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
@@ -14,6 +14,12 @@ const Document = () => {
         <link
           // consider 'display=swap' instead (default)
           href="https://fonts.googleapis.com/css2?family=Bodoni+Moda:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500&display=optional"
+          rel="stylesheet"
+        /> */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700&family=Playfair+Display:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
       </Head>
