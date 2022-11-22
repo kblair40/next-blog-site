b72ad1a52c84ae848b75a52524d88a584633ff78
Kevin Blair <kblair40@gmail.com>
2022-11-22 06:21:18 -0600

    Remove NavBanner component

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -2,17 +2,12 @@ import React from "react";
 import { ToastContainer } from "react-toastify";
 
 import MobileNav from "components/Navbar/MobileNav";
-import NavBanner from "components/Navbar/NavBanner";
 import Navbar from "components/Navbar";
 
 const layout = ({ children }) => {
   return (
     <div id="layout" className="overflow-hidden bg-creme min-h-screen">
       <ToastContainer />
-
-      <div className="absolute z-20">
-        <NavBanner />
-      </div>
 
       <Navbar />
 
