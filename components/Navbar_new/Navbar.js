7218f1c88b28286d5fd49b65b7a3f327ce451392
Kevin Blair <kblair40@gmail.com>
2022-11-22 07:53:17 -0600

    Finish NavLink styles

--- components/Navbar_new/Navbar.js
+++ components/Navbar_new/Navbar.js
@@ -1,19 +1,46 @@
 import React from "react";
-import { Box, Flex, Text, Heading } from "@chakra-ui/react";
+import { Box, Flex, Text, Heading, Center } from "@chakra-ui/react";
+import Link from "next/link";
+
+import { navLinks } from "./links";
 
 const Navbar = () => {
   return (
     <Box>
       <TextLogo />
+
+      <Flex justify="space-evenly" h="50px">
+        {navLinks.map((linkObj, i) => {
+          return <NavLink linkObj={linkObj} key={i} />;
+        })}
+      </Flex>
     </Box>
   );
 };
 
 export default Navbar;
 
+const NavLink = ({ linkObj }) => {
+  return (
+    <Box
+      flex={1}
+      h="100%"
+      px="2px"
+      border="1px solid #424242"
+      borderRight="none"
+    >
+      <Link href={linkObj.to}>
+        <Center h="100%" fontSize="sm" textAlign="center">
+          {linkObj.label}
+        </Center>
+      </Link>
+    </Box>
+  );
+};
+
 const TextLogo = () => {
   return (
-    <Flex align="center" direction="column" pt="60px">
+    <Flex align="center" direction="column" pt="60px" mb="58px">
       <Heading
         color="brand.darkgreen"
         fontFamily="Playfair Display"
@@ -21,6 +48,7 @@ const TextLogo = () => {
         fontSize={{ base: "36px", sm: "48px", md: "64px", lg: "72px" }}
         lineHeight="normal"
         mb="10px"
+        fontWeight="800"
       >
         Money and Other Things
       </Heading>
--- pages/_document.js
+++ pages/_document.js
@@ -19,7 +19,7 @@ const Document = () => {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700&family=Playfair+Display:wght@400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700&family=Playfair+Display:wght@400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
       </Head>
